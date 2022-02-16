61afb3ff3b72f3a75c2c8c8b50d884f56282b24c
Artsiom <kabanproff@gmail.com>
2022-02-16 10:18:56 +0300

    add in LifeCycle events,useEffect

--- src/components/Main/ForTasks/TaskContent/TaskItems/LifeCycleAxios/LifeCycleAxios.jsx
+++ src/components/Main/ForTasks/TaskContent/TaskItems/LifeCycleAxios/LifeCycleAxios.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import axios from 'axios';
-// import { NavLink } from 'react-router-dom'
+import VisibleComp from "./VisibleComp";
 
-class LifeCycleAxios extends React.Component {
+// function retCountChild(count) {
+// 	console.log(`--CildCount - ${count}--`)
+// 	// return count;
+// }
+
+
+class LifeCycleAxios extends React.PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
-			user: {}
+			user: {},
+			numbers: 0,
+			visibleComp: true,
+			childCount: 0,
+			inpVal: '',
 		}
 	}
 
@@ -16,12 +26,30 @@ class LifeCycleAxios extends React.Component {
 		this.setState({ user: data.data })
 	}
 
+	retCountChild = (count) => {
+		console.log(`--CildCount - ${count}--`)
+		// return count;
+		this.setState(() => ({ childCount: count }))
+	}
+
 	componentDidMount() {
 		console.log('----didmount----')
 		this.fetchData()
 	}
 
+	componentDidUpdate() {
+		console.log('--- didUpdate---')
+		console.log(this.state)
 
+	}
+
+	// shouldComponentUpdate() {
+	// 	return this.state.numbers === 0 ? false : true
+	// }
+
+	toggle() {
+		this.state.visibleComp ? this.setState(() => ({ visibleComp: false })) : this.setState(() => ({ visibleComp: true }))
+	}
 
 	render() {
 		console.log('-render-')
@@ -33,9 +61,25 @@ class LifeCycleAxios extends React.Component {
 				<div>My name is {name}</div>
 				<div>I'm from in {location}</div>
 				<div><a href={html_url} target='_blank' rel="noreferrer"> My GIT</a></div>
-			</div>
+				<button onClick={() => this.setState(state => ({ numbers: state.numbers + 1 }))}>{this.state.numbers} DIDuPDATE</button>
+
+				<div>{this.state.childCount}</div>
+				<div>{this.state.visibleComp && <VisibleComp reCou={this.retCountChild} oldCou={this.state.childCount} />}</div>
+
+				<button onClick={this.toggle.bind(this)}>visible not visible</button>
+				<input
+					type={'text'}
+					onChange={e => {
+						console.log(this.state.inpVal);
+						this.setState(() => ({ inpVal: e.target.value }))
+					}}
+					value={this.state.inpVal} />
+				<div>{this.state.inpVal}</div>
+			</div >
 		)
 	}
 }
 
-export default React.memo(LifeCycleAxios);
+
+
+export default LifeCycleAxios;
---
+++ src/components/Main/ForTasks/TaskContent/TaskItems/LifeCycleAxios/VisibleComp.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react"
+
+
+const VisibleComp = ({ reCou, oldCou }) => {
+
+	const [count, setCount] = useState(oldCou)
+
+	useEffect(() => {
+		console.log(`--VisibleCompMount--`)
+		return () => {
+			console.log(`--VisibleCompWillUnmount- ${count}-`)
+
+		}
+	}
+		, [])
+
+	useEffect(() => {
+		console.log('--VisCompUpdate--')
+		reCou(count)
+	})
+
+	return (<>
+
+		<div>VisibleComponent count = {count}</div>
+		<button onClick={() => setCount(count + 1)}>Add count +1</button>
+	</>
+	)
+}
+
+export default React.memo(VisibleComp)
